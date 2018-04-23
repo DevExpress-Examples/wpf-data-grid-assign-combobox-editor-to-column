8f0b86f7495aa1527c54f1633b803a1854988b3b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/DXGrid_AssignComboBoxToColumn/Window1.xaml.vb
@@ -0,0 +1,30 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace DXGrid_AssignComboBoxToColumn
+	Partial Public Class Window1
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+	Public Class NwindData
+		Public Shared ReadOnly Property Data() As nwindDataSet.ProductsDataTable
+			Get
+				Return New nwindDataSetTableAdapters.ProductsTableAdapter().GetData()
+			End Get
+		End Property
+	End Class
+End Namespace
