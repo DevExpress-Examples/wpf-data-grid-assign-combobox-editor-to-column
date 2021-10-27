56bbf68f98acab050ab871127d5e0f770e8fb648
CORP\ploskin.alexander <alexander.ploskin@devexpress.com>
2021-10-27 18:56:08 +0300

    Fix codestyle

---
+++ CS/DXGrid_AssignComboBoxToColumn/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DXGrid_AssignComboBoxToColumn {
+    public class Person {
+        public string ProductName { get; set; }
+        public int Type { get; set; }
+        public int UnitPrice { get; set; }
+
+        public Person(int i) {
+            ProductName = "FirstName" + i;
+            UnitPrice = i;
+            Type = 0;
+        }
+    }
+
+    public class ProductType {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+
+        public ProductType(int i) {
+            Id = i;
+            TypeName = "Type" + i;
+        }
+    }
+
+    public class MyViewModel {
+        public List<Person> PersonList { get; set; }
+        public List<ProductType> TypeList { get; set; }
+
+        public MyViewModel() {
+            PersonList = new List<Person>(GetPersonData(20));
+            TypeList = new List<ProductType>(GetTypeData(3));
+        }
+
+        static IEnumerable<Person> GetPersonData(int count) {
+            return Enumerable.Range(0, count).Select(x => new Person(x));
+        }
+
+        static IEnumerable<ProductType> GetTypeData(int count) {
+            return Enumerable.Range(0, count).Select(x => new ProductType(x));
+        }
+    }
+}
---
+++ CS/DXGrid_AssignComboBoxToColumn/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace DXGrid_AssignComboBoxToColumn {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+    }
+}
