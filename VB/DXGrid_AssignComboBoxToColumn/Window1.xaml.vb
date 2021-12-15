e12763c07f08353fac8e4f47dc36974b54c75f1a
DevExpressExampleBot <fakeEmail>
2021-12-15 19:16:02 +0300

    Source auto update [skip ci]

--- VB/DXGrid_AssignComboBoxToColumn/Window1.xaml.vb
+++ VB/DXGrid_AssignComboBoxToColumn/Window1.xaml.vb
@@ -1,55 +1,65 @@
-﻿Imports System.Collections.ObjectModel
+Imports System.Collections.ObjectModel
 Imports System.Windows
 
 Namespace DXGrid_AssignComboBoxToColumn
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            Me.DataContext = New MyViewModel()
+            Me.InitializeComponent()
+            DataContext = New MyViewModel()
         End Sub
     End Class
+
     Public Class MyViewModel
+
         Public Sub New()
             CreateList()
         End Sub
 
-        Public Property PersonList() As ObservableCollection(Of Person)
-        Public Property TypeList() As ObservableCollection(Of ProductType)
+        Public Property PersonList As ObservableCollection(Of Person)
+
+        Public Property TypeList As ObservableCollection(Of ProductType)
+
         Private Sub CreateList()
             PersonList = New ObservableCollection(Of Person)()
-            For i As Integer = 0 To 19
-                Dim p As New Person(i)
+            For i As Integer = 0 To 20 - 1
+                Dim p As Person = New Person(i)
                 PersonList.Add(p)
-            Next i
+            Next
+
             TypeList = New ObservableCollection(Of ProductType)()
-            For i As Integer = 0 To 2
+            For i As Integer = 0 To 3 - 1
                 TypeList.Add(New ProductType(i))
-            Next i
+            Next
         End Sub
     End Class
+
     Public Class Person
+
         Public Sub New(ByVal i As Integer)
             ProductName = "FirstName" & i
-
             UnitPrice = i
             Type = 0
         End Sub
 
-        Public Property ProductName() As String
+        Public Property ProductName As String
 
-        Public Property Type() As Integer
+        Public Property Type As Integer
 
-        Public Property UnitPrice() As Integer
+        Public Property UnitPrice As Integer
     End Class
 
     Public Class ProductType
+
         Public Sub New(ByVal i As Integer)
             Id = i
             TypeName = "Type" & i
         End Sub
-        Public Property Id() As Integer
-        Public Property TypeName() As String
+
+        Public Property Id As Integer
+
+        Public Property TypeName As String
     End Class
 End Namespace
